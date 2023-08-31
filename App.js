aed253d12fec2c4e88a750013e7a459002978dcc
Jackson Ludwig <jludwig@greenactionstudio.com>
2023-08-31 16:41:37 -0400

    add expo updates to repro

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { requestForegroundPermissionsAsync } from "expo-location";
+import { getCurrentPositionAsync } from "expo-location";
 import * as Notifications from "expo-notifications";
+import * as Updates from "expo-updates";
+import { useRef, useEffect } from "react";
+import { AppState } from "react-native";
+
+export const useRootEnteredForegroundEffect = (onForegroundEntered) => {
+  const state = useRef(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', async (nextAppState) => {
+      try {
+        console.log("prev:", state.current.match, "next:", nextAppState)
+        if (state.current.match(/inactive|background/) && nextAppState === 'active')
+          await onForegroundEntered?.();
+      } catch (err) {
+        console.error(err);
+      }
+
+      state.current = nextAppState;
+    });
+
+    return () => subscription.remove();
+  }, [onForegroundEntered]);
+};
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -12,7 +35,12 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
-  requestForegroundPermissionsAsync();
+  getCurrentPositionAsync().catch((_err) => console.log('could not get location (expected)'));
+
+  useRootEnteredForegroundEffect(async () => {
+    await Updates.reloadAsync();
+    console.log('may not log (expected)');
+  });
 
   return (
     <View style={styles.container}>
